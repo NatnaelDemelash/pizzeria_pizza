517fa9f23cb51c8b3951ca81d9b2872c39e7ecb7
Natnael Demelash <natidemelash074@gmail.com>
2024-01-13 15:58:23 +0300

    fetching data with react router loaders

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,10 +1,19 @@
 import { useLoaderData } from "react-router-dom";
 import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 function Menu() {
   const menu = useLoaderData();
   console.log(menu);
 
-  return <h1>Menu</h1>;
+  return (
+    <div>
+      <ul>
+        {menu.map((pizza) => (
+          <MenuItem pizza={pizza} key={pizza.id} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 // instante data fetching function which will be called when the menu is page loaded

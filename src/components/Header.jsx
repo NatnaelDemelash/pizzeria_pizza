f4ba3b4d3bec995dcc6b4a83bef51edf5cf070b3
Natnael Demelash <natidemelash074@gmail.com>
2024-01-15 00:44:31 +0300

    started to style the app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import UserName from "../features/user/UserName";
 
 const Header = () => {
   return (
-    <div>
+    <header className="bg-[#7c3aed] uppercase tracking-widest text-stone-300 px-4 py-3 border-b border-stone-200 sm:px-6 flex justify-between items-center">
       <Link to="/">Pizzeria Pizza co.</Link>
       <SearchOrder />
-      <p>John</p>
-    </div>
+      <UserName />
+    </header>
   );
 };
 
---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserName = () => {
+  return (
+    <div className="text-sm font-semibold hidden md:block">
+      <p>Natnael</p>
+    </div>
+  );
+};
+
+export default UserName;
